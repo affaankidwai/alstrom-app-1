0e3b83085f9943359e0b9bcb18b8651c5cd02ea7
Affaan Kidwai <kidwaiaffaan@gmail.com>
2024-07-12 15:15:07 +0530

    new change1

--- src/main/java/com/training/bms/AlstromApp1Application.java
+++ src/main/java/com/training/bms/AlstromApp1Application.java
@@ -2,13 +2,22 @@ package com.training.bms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class AlstromApp1Application {
+	
+	@GetMapping("enjoyWeekend")  
+	public String weekend() {
+		return "Saturday and Sunday";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AlstromApp1Application.class, args);
 		System.out.println("Alstrom has been started");
+		
 		
 	}
 
