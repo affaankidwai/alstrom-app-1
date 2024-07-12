f408b86ae08eae2b9959bc5a3761d378ce371d4c
Affaan Kidwai <kidwaiaffaan@gmail.com>
2024-07-12 12:42:28 +0530

    changes

--- src/main/java/com/training/bms/AlstromApp1Application.java
+++ src/main/java/com/training/bms/AlstromApp1Application.java
@@ -8,7 +8,7 @@ public class AlstromApp1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AlstromApp1Application.class, args);
-		System.out.println("Alstrom started");
+		System.out.println("Alstrom has been started");
 		
 	}
 
