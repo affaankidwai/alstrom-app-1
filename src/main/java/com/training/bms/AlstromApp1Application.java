cbdbb153c025ad080e4085253a483279c7153ae0
Affaan Kidwai <kidwaiaffaan@gmail.com>
2024-07-12 12:12:24 +0530

    print stmt

---
+++ src/main/java/com/training/bms/AlstromApp1Application.java
@@ -0,0 +1,14 @@
+package com.training.bms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlstromApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlstromApp1Application.class, args);
+		System.out.println("Alstrom app started");
+	}
+
+}
---
+++ src/test/java/com/training/bms/AlstromApp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.bms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlstromApp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
